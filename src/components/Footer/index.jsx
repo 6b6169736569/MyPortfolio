f2471da512cb857dff4cf5060bf7f5818863ab35
6b6169736569 <kaisei.suzuki.73@gmail.com>
2022-07-24 01:54:30 +0900

    add resume and set account.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ import Styles from './styles.module.scss';
 function Footer() {
 	return (
 		<footer className={Styles.footer}>
-			&copy; {new Date().getFullYear()} Jeanine White
+			&copy; {new Date().getFullYear()} 6b6169736569
 			<small className={Styles.byline}>🚀 Built by Astro</small>
 		</footer>
 	);
